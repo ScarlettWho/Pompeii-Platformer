d5ef89a2d879ff75df8b3669cff546fbe83b30bb
ScarlettWho <sarlett@knights.ucf.edu>
2015-10-20 11:44:40 -0400

    Update pompeii_game.js

--- pompeii_game.js
+++ pompeii_game.js
@@ -29,8 +29,8 @@ function Level(blueprint) {
 				fieldType = "wall";
 			else if (ch == "b")
 				fieldType = "bush";
-			//else if (ch == "s")
-				//fieldType = "vine";
+			else if (ch == "s")
+				fieldType = "vine";
 			//else if (ch == "c")
 				//fieldType = "door";
 			gridLine.push(fieldType);
@@ -42,6 +42,19 @@ function Level(blueprint) {
 	//})[0];
 }
 
+//What does this vector function refer to and control?
+function Vector(x, y) {
+	this.x = x; this.y = y;
+}
+
+Vector.prototype.plus = function(other) {
+	return new Vector(this.x + other.x, this.y + other.y);
+}
+
+Vector.prototype.times = function(factor) {
+	return new Vector(this.x * factor, this.y * factor);
+}
+
 function elmnt(name,className) {
 	var elmnt = document.createElement(name);
 	if (className) elmnt.className = className;
@@ -51,6 +64,8 @@ function elmnt(name,className) {
 function DOMDisplay(parent, level) {
 	this.wrap = parent.appendChild(elmnt("div", "game"));
 	this.level = level;
+	this.pos = new Vector(0, -0.5);
+	this.speed = new Vector(0,0);
 	this.wrap.appendChild(this.drawBackground());
 }
 
@@ -67,6 +82,30 @@ DOMDisplay.prototype.drawBackground = function() {
 		});
 	});
 	return table;
+};
+
+var scrollXSpeed = 100;
+var scrollYSpeed = 100;
+
+DOMDisplay.prototype.scrollView = function(keys, step) {
+	var width = this.wrap.clientWidth;
+	var maxWidth = this.wrap.scrollWidth;
+	var height = this.wrap.clientHeight;
+	var maxHeight = this.wrap.scrollHeight;
+
+	this.speed.x = 0;
+	if (keys.left && this.pos.x > 0) this.speed.x -= scrollXSpeed;
+	if (keys.right && (this.pos.x < (maxWidth - width))) this.speed.x += scrollXSpeed;
+
+	this.speed.y = 0;
+	if (keys.up && this.pos.y > 0) this.speed.y -= scrollYSpeed;
+	if (keys.down && (this.pos.y < (maxHeight - height))) this.speed.y += scrollYSpeed;
+
+	var motion = new Vector(this.speed.x * step, this.speed.y * step);
+	var newPos = this.pos.plus(motion);
+	this.wrap.scrollLeft = newPos.x;
+	this.wrap.scrollTop = newPos.y;
+	this.pos = newPos;
 };
 
 function runLevel(level, Display) {
